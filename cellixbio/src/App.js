65592902a0f4fc1f59d3a3e7d4530cd580efd269
ssvamsee <siddamvamsee@gmail.com>
2022-11-01 15:07:35 +0530

    nothing

--- cellixbio/src/App.js
+++ cellixbio/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import NavBar from './Components/Navbar/Navbar';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar></Navbar>
+      <NavBar></NavBar>
       </BrowserRouter>
     </div>
   );
