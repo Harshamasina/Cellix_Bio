0296a7ebcaaac437cbeb34166ced16a9548485f3
Harshamasina <18uec021@lnmiit.ac.in>
2022-11-03 17:28:58 +0530

    Changed Footer

---
+++ cellixbio/src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import NOP from "./NOP";
+import Social from "./Social";
+
+function Footer(){
+    <div>
+        <h1>Footer</h1>
+        <NOP></NOP>
+        <Social></Social>
+    </div>
+}
+export default Footer;
