f231fe314b7f9bde3b5d632cbb39a24e4b3373fd
Harshamasina <18uec021@lnmiit.ac.in>
2022-11-03 09:57:31 +0530

    changed contact and Leadership

--- cellixbio/src/Components/Navbar/Contact.js
+++ cellixbio/src/Components/Navbar/Contact.js
@@ -30,7 +30,46 @@ function Contact(){
 
     return(
         <div>
-            <h3 className='CUh3'>CONTACT US</h3>
+            <h3 className='CUh2'>CONTACT US</h3>
+
+            <div className='CUContainer'>
+                <div className='CUOOA'>
+                    <h3 className='CUh3'>Our Office Address</h3>
+                    <h4 className='CUh4'>Cellix Bio Private Limited</h4>
+                    <ul className='CUOOAUL'>
+                        <li>Plot No.1177 B, Road No.56,</li>
+                        <li>Jubilee Hills, Hyderabad,</li>
+                        <li>Pin: 500033, <span className='CUSpan'>India.</span></li>
+                    </ul>
+                    <h4 className='CUh4'>Cellix Biosciences Inc,</h4>
+                    <ul className='CUOOAUL'>
+                        <li>NJIT-Enterprise Development Center,</li>
+                        <li>211 Warren Street, Newark, NJ 07103,</li>
+                        <li><span className=''>USA.</span></li>
+                    </ul>
+                </div>
+       
+                <div className='CUCN'>
+                   <h3 className='CUh3'>Contact No.</h3>
+                   <ul className='CUCNUL'>
+                        <li>Phone: (+91) 40 23545946</li>
+                        <li>Fax: (+91) 40 23545949</li>
+                        <li>Email: info@cellixbio.com</li> <br></br><br></br><br></br>
+                        <li>Phone: +1 862 485 7489</li> 
+                        <li>Email: info@cellixbio.com</li>
+                   </ul> 
+                </div>
+
+                <div className='CUBH'>
+                    <h3 className='CUh3'>Business Hours</h3>
+                    <ul className='CUBHUL'>
+                        <li><span>Monday - Saturday</span></li>
+                        <li>9:30am to 6pm</li>
+                        <li>Sunday - Closed</li>
+                    </ul>
+                </div>
+            </div>
+            
             <div className='CUFormContainer'>
                 <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
--- cellixbio/src/Components/Navbar/Leadership.js
+++ cellixbio/src/Components/Navbar/Leadership.js
@@ -5,16 +5,20 @@ function Leadership(){
             <div>
                 <h4 className="Lh4">Management Team</h4>
             </div>
-            {/* <div>
-                <img src="https://www.cellixbio.com/images/team/MaheshKandula1.jpg" alt="L1"></img>
-            </div> */}
-            <div className="LulContainer">
-                <ul className="Lul">
-                    <li>Mahesh Kandula</li>
-                    <li>Director</li>
-                    <li>Drug Design and Development, Innovation & IP</li>
-                    <li>Founder</li>
-                </ul>
+
+            <div className="LFlex">
+                <div className="LImageDiv">
+                <img className="LImage" src="https://www.cellixbio.com/images/team/MaheshKandula1.jpg" alt="L1"></img>
+                </div>
+
+                <div className="LDetails">
+                    <ul className="Lul">
+                        <li>Mahesh Kandula</li>
+                        <li>Director</li>
+                        <li>Drug Design and Development, Innovation & IP</li>
+                        <li>Founder</li>
+                    </ul>
+                </div>
             </div>
             <div className="LpContainer">
                 <p className="Lp">Mahesh brings over 18 years of scientific and entrepreneurial experience in the life science industry. Mr. Kandula has been named an inventor on 72 United States Issued Patents, 29 United States Patent Pending Applications and more than 226 International Patent Applications. Mr. Kandula received his Master of Technology degree from IIT Kharagpur.</p>
