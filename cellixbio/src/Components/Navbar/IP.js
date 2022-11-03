61c75b0a42b5b3d368b2e6fbbbeaa27df788958a
Harshamasina <18uec021@lnmiit.ac.in>
2022-11-03 16:11:22 +0530

    changed IP

--- cellixbio/src/Components/Navbar/IP.js
+++ cellixbio/src/Components/Navbar/IP.js
@@ -25,109 +25,109 @@ function IP(){
                                  </thead>
                                  <tbody>
                         <tr>
-                        <td>1</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF EPILEPSY</td>
-                        <td>92,90,486</td>
+                            <td>1</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF EPILEPSY</td>
+                            <td>92,90,486</td>
                         </tr>
                         <tr>
-                        <td>2</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF DEPRESSION</td>
-                        <td>93,99,634</td>
+                            <td>2</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF DEPRESSION</td>
+                            <td>93,99,634</td>
                         </tr>
                         <tr>
-                        <td>3</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF INFLAMMATORY BOWEL DISEASE</td>
-                        <td>94,98,461</td>
+                            <td>3</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF INFLAMMATORY BOWEL DISEASE</td>
+                            <td>94,98,461</td>
                         </tr>
                         <tr>
-                        <td>4</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF INFLAMMATORY BOWEL DISEASE</td>
-                        <td>93,33,187</td>
+                            <td>4</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF INFLAMMATORY BOWEL DISEASE</td>
+                            <td>93,33,187</td>
                         </tr>
                         <tr>
-                        <td>5</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF MULTIPLE SCLEROSIS</td>
-                        <td>95,80,383</td>
+                            <td>5</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF MULTIPLE SCLEROSIS</td>
+                            <td>95,80,383</td>
                         </tr>
                         <tr>
-                        <td>6</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF METABOLIC DISORDERS</td>
-                        <td>95,22,884</td>
+                            <td>6</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF METABOLIC DISORDERS</td>
+                            <td>95,22,884</td>
                         </tr>
                         <tr>
-                        <td>7</td>
-                        <td>PRODRUGS OF ANTI-PLATELET AGENTS</td>
-                        <td>91,75,008</td>
+                            <td>7</td>
+                            <td>PRODRUGS OF ANTI-PLATELET AGENTS</td>
+                            <td>91,75,008</td>
                         </tr>
                         <tr>
-                        <td>8</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF HYPERGLYCEMIA</td>
-                        <td>91,50,557</td>
+                            <td>8</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF HYPERGLYCEMIA</td>
+                            <td>91,50,557</td>
                         </tr>
                         <tr>
-                        <td>9</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF METABOLIC SYNDROME</td>
-                        <td>93,21,716</td>
+                            <td>9</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF METABOLIC SYNDROME</td>
+                            <td>93,21,716</td>
                         </tr>
                         <tr>
-                        <td>10</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF EPILEPSY</td>
-                        <td>92,90,486</td>
+                            <td>10</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF EPILEPSY</td>
+                            <td>92,90,486</td>
                         </tr>
                         <tr>
-                        <td>11</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF LOCAL PAIN</td>
-                        <td>91,73,877</td>
+                            <td>11</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF LOCAL PAIN</td>
+                            <td>91,73,877</td>
                         </tr>
                         <tr>
-                        <td>12</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF MODERATE TO SEVERE PAIN</td>
-                        <td>91,08,942</td>
+                            <td>12</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF MODERATE TO SEVERE PAIN</td>
+                            <td>91,08,942</td>
                         </tr>
                         <tr>
-                        <td>13</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF MODERATE TO SEVERE PAIN</td>
-                        <td>94,03,793</td>
+                            <td>13</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF MODERATE TO SEVERE PAIN</td>
+                            <td>94,03,793</td>
                         </tr>
                         <tr>
-                        <td>14</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF METABOLIC SYNDROME</td>
-                        <td>93,15,478</td>
+                            <td>14</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF METABOLIC SYNDROME</td>
+                            <td>93,15,478</td>
                         </tr>
                         <tr>
-                        <td>15</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF PARKINSON'S DISEASE</td>
-                        <td>92,66,823</td>
+                            <td>15</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF PARKINSON'S DISEASE</td>
+                            <td>92,66,823</td>
                         </tr>
                         <tr>
-                        <td>16</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF INFLAMMATORY DISORDERS</td>
-                        <td>94,03,826</td>
+                            <td>16</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF INFLAMMATORY DISORDERS</td>
+                            <td>94,03,826</td>
                         </tr>
                         <tr>
-                        <td>17</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF NEUROLOGICAL DEGENERATIVE DISORDERS</td>
-                        <td>94,34,704</td>
+                            <td>17</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF NEUROLOGICAL DEGENERATIVE DISORDERS</td>
+                            <td>94,34,704</td>
                         </tr>
                         <tr>
-                        <td>18</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF BLOOD CLOTTING DISORDERS</td>
-                        <td>93,09,233</td>
+                            <td>18</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF BLOOD CLOTTING DISORDERS</td>
+                            <td>93,09,233</td>
                         </tr>
                         <tr>
-                        <td>19</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF NEUROLOGIC DISEASES</td>
-                        <td>93,15,461</td>
+                            <td>19</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF NEUROLOGIC DISEASES</td>
+                            <td>93,15,461</td>
                         </tr>
                         <tr>
-                        <td>20</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF SEVERE PAIN</td>
-                        <td>95,73,927</td>
+                            <td>20</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF SEVERE PAIN</td>
+                            <td>95,73,927</td>
                         </tr>
                         <tr>
-                        <td>21</td>
-                        <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF NEUROLOGICAL CONDITIONS</td>
-                        <td>92,33,161</td>
+                            <td>21</td>
+                            <td>COMPOSITIONS AND METHODS FOR THE TREATMENT OF NEUROLOGICAL CONDITIONS</td>
+                            <td>92,33,161</td>
                         </tr>
                         <tr>
                             <td>22</td>
