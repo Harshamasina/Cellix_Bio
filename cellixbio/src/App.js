d3603982b7209ab0d40cb0dcc742ffc7442e7c29
ssvamsee <siddamvamsee@gmail.com>
2022-11-04 15:41:15 +0530

    added footer

--- cellixbio/src/App.js
+++ cellixbio/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './Components/Navbar/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
       <NavBar></NavBar>
       </BrowserRouter>
+      <Footer></Footer>
     </div>
   );
 }
