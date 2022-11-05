245082abd60ef20d64e5e5db5e578c8d67f00c38
ssvamsee <siddamvamsee@gmail.com>
2022-11-05 18:53:48 +0530

    added Modal

--- cellixbio/src/App.js
+++ cellixbio/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Components/Footer/Footer';
 import React from 'react';
+import ModalDialog from './Components/Body/Intro/ModalDialog';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <NavBar></NavBar>
       </BrowserRouter>
       <Footer></Footer>
+      <ModalDialog/>
     </div>
   );
 }
