0ef91ab04967af406f183d69df0bcfee02fc46c4
ssvamsee <siddamvamsee@gmail.com>
2022-11-10 19:47:27 +0530

    updated footer to spa

--- cellixbio/src/App.js
+++ cellixbio/src/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <NavBar></NavBar>
+        <Footer></Footer>
       </BrowserRouter>
-      <Footer></Footer>
       
     </div>
   );
