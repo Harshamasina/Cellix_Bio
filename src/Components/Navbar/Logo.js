801f0c67b01e875390f3775b1a951c19077e6173
ssvamsee <siddamvamsee@gmail.com>
2022-11-11 16:51:04 +0530

    logo

--- src/Components/Navbar/Logo.js
+++ src/Components/Navbar/Logo.js
@@ -4,7 +4,6 @@ function Logo(){
     return(
         <div className="logo">
             <NavLink to="/Home">
-            
             <img className="logo" src="https://www.cellixbio.com/images/logo-dark.png" alt="Logo"></img></NavLink>
         </div>
     )
