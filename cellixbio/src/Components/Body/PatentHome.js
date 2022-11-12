aba84a8a6b266b6fc90fbd21d96e483bb9c97edf
Harshamasina <18uec021@lnmiit.ac.in>
2022-11-12 17:02:01 +0530

    changed patenthome

--- cellixbio/src/Components/Body/PatentHome.js
+++ cellixbio/src/Components/Body/PatentHome.js
@@ -1,11 +1,13 @@
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 function PatentHome(){
+    const navigate = useNavigate();
     return (
         <div>
             <div className='PatentHomeContainer'>
                 <img className='PatentHomeImg' src="https://pharmaceuticalmanufacturer.media/downloads/8719/download/shutterstock_1952492305.jpg?cb=6a0449b64584f77e8d4d0bb7585d324f&w=1000&h=" alt="hands"></img>
-                <p className='PatentHomep'>CELLIX BIO's HAS OVER 432 GLOBAL PATENTS FROM 2009 to 2022</p>
-                <Button variant="outline-info"className='PatentHomeButton'>Know More</Button>
+                <p className='PatentHomep'>CELLIX BIO's HAS OVER 432 GLOBAL PATENTS FROM 2022 TO 2009</p>
+                <Button onClick={() => navigate('/Patents')} variant="outline-info" className='PatentHomeButton'>Know More</Button>
             </div>
         </div>
     );
