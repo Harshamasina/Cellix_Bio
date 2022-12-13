d650cb412ed70152c843829db40e708349f23a83
Harshamasina <18uec021@lnmiit.ac.in>
2022-12-13 08:52:54 +0530

    changed

--- cellixbio/src/Components/Body/Patents/Years/TherapeuticArea.js
+++ cellixbio/src/Components/Body/Patents/Years/TherapeuticArea.js
@@ -5,7 +5,7 @@ function TherapeuticArea(props){
     return(
         <div>
             <div className="TAContainer">
-               <h2 className="PITBh3">THERAPEUTIC AREA</h2>
+               {/* <h2 className="PITBh3">THERAPEUTIC AREA</h2> */}
                <h3 className="PITBh4">{props.therapeuticArea}</h3>
                <div className="PITBpContainer">
                   <p className="PITBp">{props.diseases}</p>
