dcd75abcf3c4bb4eaf7b13be0f027cbb4b4027d9
Harshamasina <18uec021@lnmiit.ac.in>
2022-12-26 17:01:00 +0530

    added leadership

---
+++ cellixbio/src/Components/Navbar/Inventor.js
@@ -0,0 +1,31 @@
+// import Card from 'react-bootstrap/Card';
+// import CardGroup from 'react-bootstrap/CardGroup';
+
+function Inventor() {
+  document.title = 'Leadership - Cellix Bio';
+  return (
+           <div className='LeaderContainer'>
+            <div className='LeaderContainer1'>
+              <div className='LeaderImgContainer'>
+                  <img className='LeaderImg' src="https://cellixbio-assets.s3.ap-south-1.amazonaws.com/Web+Images/CellixBio.Leadership.Mahesh.PNG" alt='Limage'/>
+              </div>
+              <div className='LeaderTitleContainer'>
+                  <h1 className='LeaderTitle'>Mahesh Kandula</h1><p className='Leadertext'>Inventor</p>
+              </div>
+            </div>
+            <div className='LeaderContainer2'>
+              <div className='LeaderSubTitleContainer'> 
+                <div className='DescContainerh3'><span></span>
+                  <span className="WWDh3">About Mahesh Kandula</span><span></span>
+                </div>
+              </div>  
+                            <hr className='LeaderHR'></hr>
+                <div className='LeaderMsgContainer'>
+                  <p className='LeaderMsg'>Mahesh brings over 18 years of scientific and entrepreneurial experience in the life science industry. Mr. Kandula has been named an inventor on 112 United States Patents and Applications and, more than 635 International Patent Applications. Mr. Kandula received his Master of Technology degree from IIT Kharagpur.</p>
+                </div>
+            </div>
+          </div>   
+  );
+        }
+
+export default Inventor;
