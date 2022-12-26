4977b397b7c0b8d1a63e30a54ee61c5626f797f4
ssvamsee <siddamvamsee@gmail.com>
2022-12-26 18:42:02 +0530

    CSS

--- cellixbio/src/Components/Navbar/Inventor.js
+++ cellixbio/src/Components/Navbar/Inventor.js
@@ -4,6 +4,15 @@
 function Inventor() {
   document.title = 'Leadership - Cellix Bio';
   return (
+    <>
+     <div className='patentlandingpage'>
+              <video src="https://cellixbio-assets.s3.ap-south-1.amazonaws.com/Web+Images/Neurons.mp4" autoPlay muted loop className='patents_video_bg' type="video/mp4"/>
+              <div className='pipeline-text'>
+                            <div className='patents_text_1'>
+                                <h1 className='pipelineCarouselh1'>INVENTOR</h1>
+                            </div>
+                            </div>
+              </div>
            <div className='LeaderContainer'>
             <div className='LeaderContainer1'>
               <div className='LeaderImgContainer'>
@@ -24,7 +33,8 @@ function Inventor() {
                   <p className='LeaderMsg'>Mahesh brings over 18 years of scientific and entrepreneurial experience in the life science industry. Mr. Kandula has been named an inventor on 112 United States Patents and Applications and, more than 635 International Patent Applications. Mr. Kandula received his Master of Technology degree from IIT Kharagpur.</p>
                 </div>
             </div>
-          </div>   
+          </div> 
+          </>  
   );
         }
 
--- cellixbio/src/Components/Navbar/Leadership.js
+++ cellixbio/src/Components/Navbar/Leadership.js
@@ -1,8 +1,18 @@
 function Leadership(){
     return(
+      <>
+      <div className='patentlandingpage'>
+              <video src="https://cellixbio-assets.s3.ap-south-1.amazonaws.com/Web+Images/Cup.mp4" autoPlay muted loop className='patents_video_bg' type="video/mp4"/>
+              <div className='pipeline-text'>
+                            <div className='patents_text_1'>
+                                <h1 className='pipelineCarouselh1'>LEADERSHIP</h1>
+                            </div>
+                            </div>
+              </div>
         <div>
             <h1>Leadership</h1>
         </div>
+        </>
     );
 }
 export default Leadership;
