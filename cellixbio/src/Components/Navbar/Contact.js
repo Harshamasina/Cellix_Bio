1bfa94285c450aa3d033b45d106e7ced429c95d3
Harshamasina <18uec021@lnmiit.ac.in>
2022-12-30 16:06:15 +0530

    added address

--- cellixbio/src/Components/Navbar/Contact.js
+++ cellixbio/src/Components/Navbar/Contact.js
@@ -1,23 +1,26 @@
-// import Address from './Contacts/Address';
+import Address from './Contacts/Address';
 import SendUsEmail from './Contacts/SendUsEmail';
 
 function Contact() {
   document.title = 'Contact - Cellix Bio';
   return (
     <>
-    <div className='patentlandingpage'>
-              <video src="https://cellixbio-assets.s3.ap-south-1.amazonaws.com/Web+Images/Planet.mp4" autoPlay muted loop className='patents_video_bg' type="video/mp4"/>
-              <div className='pipeline-text'>
-                            <div className='patents_text_1'>
-                                <h1 className='pipelineCarouselh1'>CONTACT</h1>
-                            </div>
-                            </div>
-              </div>
-    {/* <div className='ContactCont'>
-      <div className='DescContainerh3'><span></span>
-                <span className="WWDh3">Send your Queries</span><span></span>
-                </div></div> */}
-      {/* <Address></Address> */}
+      <div className='patentlandingpage'>
+        <video src="https://cellixbio-assets.s3.ap-south-1.amazonaws.com/Web+Images/Planet.mp4" autoPlay muted loop className='patents_video_bg' type="video/mp4"/>
+        <div className='pipeline-text'>
+          <div className='patents_text_1'>
+            <h1 className='pipelineCarouselh1'>CONTACT</h1>
+          </div>
+        </div>
+      </div>
+      
+      <Address></Address>
+      
+      <div className='ContactCont'>
+        <div className='DescContainerh3'>
+          <span></span><span className="WWDh3">Send your Queries</span><span></span>
+        </div>
+      </div>
       <SendUsEmail></SendUsEmail>
     </>
   );
